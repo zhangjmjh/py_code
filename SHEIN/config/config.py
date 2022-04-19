3dd7b74d186b40c7e790da1fcc71956f215f9309
zhangjm <zhangjm.jh@qq.com>
2022-04-19 18:42:35 +0800

    重新启用git

---
+++ SHEIN/config/config.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+# 全局变量的域名
+
+class global_var:
+    app = "https://api-service-test01.shein.com"  # app测试环境
+    AppVersion = '8.0.2'
+
+def app():
+    global_var.app = "app"
+def app():
+    return global_var.app
+
+def AppVersion():
+    global_var.AppVersion = "AppVersion"
+def AppVersion():
+    return global_var.AppVersion
+
+
